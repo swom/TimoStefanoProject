3237713eeb1c50fc7f5b3cdb87ca7ab925d5b743
swom <cicciacco@gmail.com>
2020-04-09 16:34:45 +0200

    fix # 11

--- simulation.cpp
+++ simulation.cpp
@@ -10,13 +10,13 @@ simulation::simulation(double init_target_value):
 
 
 
-void test_simulation() noexcept //!OCLINT test may be many
+void test_simulation() noexcept//!OCLINT test may be many
 {
   //Checks that simulation does have a environment member
   //The value 123456789 is irrelevant is just needed to call
   //get_env()
   {
-    simulation s;
+    simulation s;//!OCLINT
     assert (s.get_env().get_target_value() < 123456789);
   }
 }
