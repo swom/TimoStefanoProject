4c73dea1218b10a733a71e570f778c5692cda99e
swom <cicciacco@gmail.com>
2020-04-30 16:56:23 +0200

    added a param class, added a target_value for environment

--- param.cpp
+++ param.cpp
@@ -2,10 +2,12 @@
 
 #include<cassert>
 
-param::param(int init_pop_size):
-    m_init_pop_size{init_pop_size}
+param::param(int init_pop_size, double target_value ):
+    m_init_pop_size{init_pop_size},
+    m_target_value{target_value}
 {
-
+assert(m_init_pop_size > 0);
+assert(m_target_value >= 0);
 }
 
 void test_param() noexcept
@@ -17,5 +19,16 @@ void test_param() noexcept
         int init_pop_size = 42;
         param p(init_pop_size);
         assert(p.get_init_pop_size() == init_pop_size);
+    }
+
+    //Parameters can be intitialized with a given value for
+    //starting target value
+    //It has to be between 0 and 1
+    //default 1
+    {
+        double target_value = 0.142;
+        param p(1,target_value);
+        assert(p.get_target_value() - target_value < 0.00001 &&
+               p.get_target_value() - target_value > - 0.00001);
     }
 }
--- param.h
+++ param.h
@@ -5,13 +5,23 @@
 class param
 {
 public:
-    param(int init_pop_size = 1);
+    param(int init_pop_size = 1, double target_value = 0);
 
     ///Retruens the number of individuals at the start of the simulation
     int get_init_pop_size() const noexcept {return m_init_pop_size;}
+
+    ///Returns the environment target_value
+    double get_target_value() const noexcept {return m_target_value;}
+
 private:
+
     ///The number of individuals at the start of the simulation
     int m_init_pop_size;
+
+    ///The target value that is gonna give the best fitness
+    ///to individuals that match it
+    double m_target_value;
+
 };
 
 void test_param() noexcept;
