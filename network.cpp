39ae4a8f53d86a164020f4f33f93d0e4c2322c6b
S. Tiso <P288427@Workspace.rug.nl>
2020-10-23 12:31:07 +0200

    fixed constructor and response, adding test to response

--- network.cpp
+++ network.cpp
@@ -4,21 +4,29 @@
 #include <cmath>
 #include <numeric>
 
-network::network(std::vector<int> nodes_per_intermediate_layer)
+network::network(std::vector<int> nodes_per_layer):
+    m_input_size{nodes_per_layer[0]}
 {
+    for (size_t i = 1; i != nodes_per_layer.size(); i++ )
+    {
+        std::vector<std::vector<double>>temp_layer_vector;
+        size_t n_nodes_prev_layer = nodes_per_layer[i-1];
+        for(int j = 0; j != nodes_per_layer[i]; j++)
+        {
+          std::vector<double> temp_weights(n_nodes_prev_layer, 0);
+          temp_layer_vector.push_back(temp_weights);
+        }
 
-    for (size_t i = 0; i != nodes_per_intermediate_layer.size()-1; i++ ){
-        int number_connections = nodes_per_intermediate_layer[i] * nodes_per_intermediate_layer[i + 1];
-        m_network_weights.push_back(std::vector <double>(number_connections, 0));
+        //A vector of the size of the number of connections is pushed back in the weight matrix
+        m_network_weights.push_back(temp_layer_vector);
     }
 }
 
 
-std::vector<double> response (const network& n, const std::vector<double>& input)
+std::vector<double> response(const network& n, const std::vector<double>& input)
 {
     std::vector<double>previous_layer_value = input;
-
-    assert(input.size() == n.get_net_weights()[0].size());
+    assert(input.size() == n.get_input_size());
 
     for(size_t i = 0; i != n.get_net_weights().size(); i++)
     {
@@ -53,19 +61,19 @@ void test_network()
         //The number of rows of the weight matrix
         //should be the number elements of the vector
         //privded to the constructor
-        // minus 1 (the output layer does not have forward connections)
+        // minus 1 (the input layer does not have connections to previous layers)
         assert(n.get_net_weights().size() == testvec.size() -1);
 
         for(size_t  i = 0; i != n.get_net_weights().size(); i++)
         {
-            assert (static_cast<int>(n.get_net_weights()[i].size()) == testvec[i]*testvec[i+1]);
+            assert (static_cast<int>(n.get_net_weights()[i].size()) == testvec[i]*testvec[i - 1]);
         }
 
     }
 
     ///The function resposne returns the output of the network
     {
-        auto very_simple_nodes = std::vector<int>{1,1};
+        auto very_simple_nodes = std::vector<int>{1,2,1};
         auto input = std::vector<double>{1};
         auto expected_output = std::vector<double>{0};
         network n{very_simple_nodes};
--- network.h
+++ network.h
@@ -10,11 +10,17 @@ public:
     network(std::vector<int> nodes_per_layer);
 
     ///Returns const ref to vector of weights
-    const std::vector<std::vector<double>>& get_net_weights() const noexcept{return m_network_weights;}
+    const std::vector<std::vector<std::vector<double>>>& get_net_weights() const noexcept{return m_network_weights;}
+
+    ///Returns the input size
+    size_t get_input_size() const noexcept {return static_cast<size_t>(m_input_size);}
 
 private:
+    ///Vector of of vectors, representing the weights coming into each node
+    std::vector<std::vector<std::vector<double>>> m_network_weights;
 
-    std::vector<std::vector<double>> m_network_weights;
+    ///The size of the input vector the network will receive
+    int m_input_size;
 };
 
 std::vector<double> response (const network& n);
