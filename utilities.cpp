0f0d1512f0ab4a5acbdc136d49c47e70a968a36c
swom <cicciacco@gmail.com>
2021-04-20 10:59:19 +0200

    adding tests for #25 and #29

--- utilities.cpp
+++ utilities.cpp
@@ -3,9 +3,9 @@
 bool are_equal(double lhs, double rhs)
 {
   return lhs - rhs < -0.0001 && lhs - rhs > 0.0001;
-};
+}
 
 bool are_not_equal(double lhs, double rhs)
 {
   return !are_equal(lhs, rhs);
-};
+}
