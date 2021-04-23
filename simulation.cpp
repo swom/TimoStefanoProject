17dc1ec479f85c881519db96057e21f4aa4c777a
swom <cicciacco@gmail.com>
2021-04-23 14:38:28 +0200

    fixing to make test pass also on github for issue #31

--- simulation.cpp
+++ simulation.cpp
@@ -229,9 +229,6 @@ void test_simulation() noexcept//!OCLINT test may be many
     auto second_ind_fit =  get_nth_ind_fitness(s,1) ;
     auto min_fit = find_min_fitness(s);
 
-    std::cout << second_ind_fit << std::endl <<
-                 min_fit << std::endl;
-
     assert(are_equal_with_tolerance(min_fit,second_ind_fit));
 
   }
