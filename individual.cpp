e7d93d5f5f8c2bc774fa9d97cc1e66badb45b2ee
swom <cicciacco@gmail.com>
2022-11-24 10:59:39 +0100

    changing input to 1 to run 1- networks

--- individual.cpp
+++ individual.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cassert>
+static double fixed_input_value = 1;
 
 individual::individual(std::vector<int> net_arch, int age) :
     m_age{age},
@@ -14,7 +15,7 @@ individual::individual(std::vector<int> net_arch, int age) :
 
 individual::individual(ind_param i_p) :
     ///!!!!Attention!!!! input values are for now a fixed amount
-    m_input_values(i_p.net_par.net_arc[0], 0),
+    m_input_values(i_p.net_par.net_arc[0], fixed_input_value),
     m_network{i_p.net_par}
 {
 
